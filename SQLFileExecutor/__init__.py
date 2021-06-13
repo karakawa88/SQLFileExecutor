d2a47b9098f4eca4bde56a7d33b39c156240eb6b
kagalpan <kagalpan@kacpp.xyz>
2021-06-13 13:35:55 +0900

    バージョンを0.2.1に変更。

--- SQLFileExecutor/__init__.py
+++ SQLFileExecutor/__init__.py
@@ -1,6 +1,6 @@
 # SQLFileExecutor
 __copyright__    = 'Copyright (C) 2021 Kentarou Arakawa'
-__version__      = '0.1.6'
+__version__      = '0.2.1'
 __license__      = 'MIT'
 __author__       = 'Kentarou Arakawa'
 __author_email__ = 'kagalpan@kacpp.xyz'
